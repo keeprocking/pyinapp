1eb4b6e81df2c2c531508429849e66faefd2db49
Ivan Mukhin <muhin.ivan@gmail.com>
2015-09-18 09:00:35 +0300

    Change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyinapp',
-    version='0.1.0',
+    version='0.1.1',
     packages=['pyinapp'],
     install_requires=['rsa', 'requests'],
     description="InApp purchase validation API wrappers",
