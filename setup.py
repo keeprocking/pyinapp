0801bdf14ede5d10b75d9adb39d0083f32d52c2f
Ivan Mukhin <muhin.ivan@gmail.com>
2017-08-27 23:16:22 +0300

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyinapp',
-    version='0.1.3',
+    version='0.1.4',
     packages=['pyinapp'],
     install_requires=['rsa', 'requests'],
     description="InApp purchase validation API wrappers",
